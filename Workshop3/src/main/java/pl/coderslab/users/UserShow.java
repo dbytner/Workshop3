b720c0e2d6ac295ca2dec0a49b4d6bfbd5926e8e
dbytner <dambyt@wp.pl>
2023-06-02 12:58:30 +0200

    fifth commit

--- Workshop3/src/main/java/pl/coderslab/users/UserShow.java
+++ Workshop3/src/main/java/pl/coderslab/users/UserShow.java
@@ -1,5 +1,8 @@
 package pl.coderslab.users;
 
+import pl.coderslab.utils.User;
+import pl.coderslab.utils.UserDAO;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,11 +12,17 @@ import java.io.IOException;
 public class UserShow extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        int idNumber = Integer.parseInt(id);
+        UserDAO userDAO = new UserDAO();
+        request.setAttribute("user", userDAO.read(idNumber));
+        getServletContext().getRequestDispatcher("/users/showUser.jsp").forward(request, response);
 
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 
     }
 }
